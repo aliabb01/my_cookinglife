6e40fe34af71d491c926281ff3393abd908b0514
aliabb01 <aliabbasov21112001@gmail.com>
2021-02-28 21:37:11 +0400

    Initial commit of the repository (Laravel Installation files + modified welcome and app.blade page)

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,127 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>MyCookingLife</title>
+
+    <meta name="description" content="MyCookingLife Şirniyyat veb səhifəsi">
+    <meta name="keywords" content="Şirniyyat, Tort, Keks, Piroq, Cake, Sweets, Dessert">
+    <meta name="author" content="Ali Abbasov">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/welcome.js') }}" defer></script>
+    <script src="https://kit.fontawesome.com/d02e083562.js" crossorigin="anonymous"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
+
+    <!-- Photoswipe links -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.3/photoswipe.min.js" integrity="sha512-2R4VJGamBudpzC1NTaSkusXP7QkiUYvEKhpJAxeVCqLDsgW4OqtzorZGpulE3eEA7p++U0ZYmqBwO3m+R2hRjA==" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.3/photoswipe-ui-default.js" integrity="sha512-7jpTN4lfrURp41NL7vGXbMP+RPaf/1S5QlNMHLlkdBteN+X5znoT2P8ryCluqePOK79rpDWVPdq1+la4ijhIbQ==" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.3/photoswipe.css" integrity="sha512-/lf2y2d7SFJau+G4TGgXCWJOAUxyDmJD+Tb35CdqoMZAQ8eNX0sgDKISlcxCtGpEAuyb1Q5vGPfB1XMettf0FA==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/photoswipe/4.1.3/default-skin/default-skin.css" integrity="sha512-QwSfZXX2w9SDWSNBKpEos673LXajTJpYKwtG+zJNP9zHsgRrWtNSx1gKVyB6qWUP4wJ0Hfnk9KJzrB6IKrXmEQ==" crossorigin="anonymous" />
+    
+</head>
+
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-info shadow-sm">
+            <div class="container">
+                <a class="brandName" href="{{ url('/') }}">
+                    MyCookingLife
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav m-3">
+                        <li class="nav-item"><a href="" class="nav-link">Ana Səhifə</a> </li>
+                        <li class="nav-item"><a href="" class="nav-link">Haqqımızda</a></li>
+                        <li class="nav-item dropdown bottom-line">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                Şirniyyatlar
+                            </a>
+
+                            <div class="dropdown-menu bg-transparent border-0" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item mb-3 dropdown-item-rounded" href="#">
+                                    Tortlar
+                                </a>
+
+                                <a class="dropdown-item mb-3 dropdown-item-rounded" href="#">
+                                    Cupcake-lər
+                                </a>
+
+                                <a class="dropdown-item mb-3 dropdown-item-rounded" href="#">
+                                    Kekslər
+                                </a>
+
+                                <a class="dropdown-item mb-3 dropdown-item-rounded" href="#">
+                                    Digərləri
+                                </a>
+                            </div>
+                        </li>
+                        <li class="nav-item"><a href="" class="nav-link">Əlaqə</a></li>
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        {{--@guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
+                        @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                        @endguest--}}
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+
+        <main class="">
+            @yield('content')
+        </main>
+        @include('layouts.footer')
+    </div>
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,247 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
+@section('content')
 
-        <title>Laravel</title>
+<div class="flex-center position-ref full-height main-container">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    <div class="container pt-5 mt-5">
+        <div class="jumbotron">
+            <h1>&lt;/&gt;</h1>
+            <p><code>&lt;/&gt;</code></p>
+        </div>
+    </div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+    <div class="container text-center">
+        <div role="tabpanel">
+            <!-- List group -->
+            <div class="list-group" role="tablist">
+                <div class="row" id="myList">
+                    <div class="col-sm category-tab">
+                        <a class="list-group-item list-group-item-action active" data-toggle="list" href="#tortlar"
+                            role="tab">Tortlar</a>
+                    </div>
+                    <div class="col-sm category-tab">
+                        <a class="list-group-item list-group-item-action" data-toggle="list" href="#cupcakeler"
+                            role="tab">Cupcake-lər</a>
+                    </div>
+                    <div class="col-sm category-tab">
+                        <a class="list-group-item list-group-item-action" data-toggle="list" href="#keksler"
+                            role="tab">Kekslər</a>
+                    </div>
+                    <div class="col-sm category-tab">
+                        <a class="list-group-item list-group-item-action" data-toggle="list" href="#cheesecakeler"
+                            role="tab">Cheesecake-lər</a>
+                    </div>
                 </div>
             </div>
+
+            <!-- Tab panes -->
+            <div class="tab-content mt-5 pb-5">
+                <div class="tab-pane active" id="tortlar" role="tabpanel">
+
+                    <div class="card-deck">
+                        <div class="card">
+                            <div class="productDiv">
+                                <img data-enlargeable
+                                    src="https://instagram.fgyd6-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/145186333_148755163629909_1744257870923970877_n.jpg?tp=1&_nc_ht=instagram.fgyd6-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=36Fa3LV3Z3MAX8O_ht0&oh=2cc204e819111988f1b86a1ffb57bbc9&oe=60640B96"
+                                    class="productImage card-img-top dovsanTort" alt="...">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Marsipanlı Tort</h5>
+                                <p class="card-text">Biskvitli marsipanlı tort.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="productDiv">
+                                <img src="https://instagram.fgyd6-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/137625644_159030845989178_7375901958313223222_n.jpg?tp=1&_nc_ht=instagram.fgyd6-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=KngtMOWqgGwAX8lmUiB&oh=70c88279014b5755c88a57e5a0020cd6&oe=606576AD"
+                                    class="productImage card-img-top spartakTort" alt="...">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Spartak</h5>
+                                <p class="card-text">Qaymaqlı şokoladlı spartak tortu.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="productDiv">
+                                <img src="https://instagram.fgyd6-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/134579862_480219392943041_6827134145626250265_n.jpg?tp=1&_nc_ht=instagram.fgyd6-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=DyRM7wdzwkAAX-HwEQY&oh=d7714fdf08c0aa959b99f1e1a6af7d5e&oe=6066841E"
+                                    class="productImage card-img-top yeniIlTort2021" alt="...">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Yeni İl Tortu</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural
+                                    lead-in to additional content. This card has even longer content than the first to
+                                    show that equal height action.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="tab-pane" id="cupcakeler" role="tabpanel">
+
+                </div>
+
+                <div class="tab-pane" id="keksler" role="tabpanel">
+
+                    <div class="card-deck">
+                        <div class="card">
+                            <div class="productDiv">
+                                <img src="https://instagram.fgyd6-1.fna.fbcdn.net/v/t51.2885-15/e35/154780182_275626787565239_8278515977738468395_n.jpg?tp=1&_nc_ht=instagram.fgyd6-1.fna.fbcdn.net&_nc_cat=111&_nc_ohc=tUAH4UMC_NwAX_QLjad&oh=2dc3a74f77d47370f06968bbe5d021f2&oe=6062BDB9"
+                                    class="productImage card-img-top ekler" alt="...">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Ekler</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="productDiv">
+                                <img src="https://instagram.fgyd6-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/127958561_221453006079336_2123881493657455603_n.jpg?tp=1&_nc_ht=instagram.fgyd6-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=DD2DvjM-6TIAX8FgMI_&oh=b9c58df7f97625895dd4e34a5f174ad3&oe=6063AF14"
+                                    class="productImage card-img-top gilemeyveliPiroq" alt="...">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Tort2</h5>
+                                <p class="card-text">This card has supporting text below as a natural lead-in to
+                                    additional content.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="productDiv">
+                                <img src="https://instagram.fgyd6-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/122656269_989157491566097_7300101664886747461_n.jpg?tp=1&_nc_ht=instagram.fgyd6-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=HWyis_tIyqAAX9xVDTh&oh=8da29485cd771b997db96c6bca6c0d24&oe=6063D1D3" class="productImage card-img-top almaliPiroq" alt="...">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Tort3</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural
+                                    lead-in to additional content. This card has even longer content than the first to
+                                    show that equal height action.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="tab-pane" id="cheesecakeler" role="tabpanel">
+
+                    <div class="card-deck">
+                        <div class="card">
+                            <div class="productDiv">
+                                <img src="https://instagram.fgyd6-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/138601371_711614296206496_8391571245981885471_n.jpg?tp=1&_nc_ht=instagram.fgyd6-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=EuolG12Kfh0AX8vJDg5&oh=fb479de441762d1050d61b91565e0efe&oe=6064940A"
+                                    class="productImage card-img-top portagalChCake" alt="...">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Portağallı çizkeyk</h5>
+                                <p class="card-text">Dadlı. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="productDiv">
+                                <img src="https://instagram.fgyd6-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/131258111_816844822194472_8076183420921678746_n.jpg?tp=1&_nc_ht=instagram.fgyd6-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=6qD0zbUeirwAX8S-u1A&oh=b666541be692dab6351766f7b7e2ea1e&oe=606415B0"
+                                    class="productImage card-img-top sokoladliChCake" alt="...">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Şokoladlı çizkeyk</h5>
+                                <p class="card-text">This card has supporting text below as a natural lead-in to
+                                    additional content.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="productDiv">
+                                <img src="https://instagram.fgyd6-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/122682811_287265682365388_4171276271498622728_n.jpg?tp=1&_nc_ht=instagram.fgyd6-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=VBAOpBXgF-MAX-sYYNG&oh=f35195f45367b2f1cc5805cc3ce179b7&oe=6063CB25"
+                                    class="productImage card-img-top ciyelekliChCake" alt="...">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Çiyələkli çizkeyk</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural
+                                    lead-in to additional content. This card has even longer content than the first to
+                                    show that equal height action.</p>
+                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
         </div>
-    </body>
-</html>
+    </div>
+
+
+    <!-- Root element of PhotoSwipe. Must have class pswp. -->
+    <div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
+
+        <!-- Background of PhotoSwipe. 
+         It's a separate element as animating opacity is faster than rgba(). -->
+        <div class="pswp__bg"></div>
+
+        <!-- Slides wrapper with overflow:hidden. -->
+        <div class="pswp__scroll-wrap">
+
+            <!-- Container that holds slides. 
+            PhotoSwipe keeps only 3 of them in the DOM to save memory.
+            Don't modify these 3 pswp__item elements, data is added later on. -->
+            <div class="pswp__container">
+                <div class="pswp__item"></div>
+                <div class="pswp__item"></div>
+                <div class="pswp__item"></div>
+            </div>
+
+            <!-- Default (PhotoSwipeUI_Default) interface on top of sliding area. Can be changed. -->
+            <div class="pswp__ui pswp__ui--hidden">
+
+                <div class="pswp__top-bar">
+
+                    <!--  Controls are self-explanatory. Order can be changed. -->
+
+                    <div class="pswp__counter"></div>
+
+                    <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
+
+                    <button class="pswp__button pswp__button--share" title="Share"></button>
+
+                    <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
+
+                    <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
+
+                    <!-- Preloader demo https://codepen.io/dimsemenov/pen/yyBWoR -->
+                    <!-- element will get class pswp__preloader--active when preloader is running -->
+                    <div class="pswp__preloader">
+                        <div class="pswp__preloader__icn">
+                            <div class="pswp__preloader__cut">
+                                <div class="pswp__preloader__donut"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
+                    <div class="pswp__share-tooltip"></div>
+                </div>
+
+                <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">
+                </button>
+
+                <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)">
+                </button>
+
+                <div class="pswp__caption">
+                    <div class="pswp__caption__center"></div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+</div>
+@endsection
