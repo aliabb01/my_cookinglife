945e2500bfb52b03dc58688c1e02b5a1e20e21a8
aliabb01 <aliabbasov21112001@gmail.com>
2021-04-04 15:48:49 +0400

    Return Geolocation data to all views on boot in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Stevebauman\Location\Facades\Location;
+use Illuminate\Support\Facades\View;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Geolocation data 
+        $locationData = Location::get();
+        View::share('locationData', $locationData); 
     }
 }
