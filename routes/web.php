2e8f9e968605892f57b56e60b78ea4d1a0b19f7c
aliabb01 <aliabbasov21112001@gmail.com>
2021-04-14 19:34:52 +0400

    Updated ROUTES. Subscription Resource Routes. Admin Routes. Composer lock file

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CampaignController;
+use App\Http\Controllers\SubscribeController;
+use App\Subscriber;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\LocationController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,15 +21,11 @@ use Illuminate\Support\Facades\Route;
 
 
 
-// Route::any('/', [LocationController::class, 'index']);
-
 Route::get('/', function () {
-    return view('welcome');   
-});
+    return view('welcome');
+})->name('home');
 
 Auth::routes();
-
-/*Route::get('/ev', 'HomeController@index')->name('welcome');*/
 
 Route::get('/ev', function () {
     return view('welcome');
@@ -34,6 +35,59 @@ Route::get('elaqe', 'ContactFormController@create');
 
 Route::post('elaqe', 'ContactFormController@store');
 
+/**
+ * Subscription Resource Routes
+ */
 
-//Route::get('sud', [LocationController::class, 'index']);
+/**
+ * View to show unsubscribe form for mailing list
+ */
+Route::get('unsubscribe/{subscribedMail}', [SubscribeController::class, 'show'])->name('subscription.show');
 
+/**
+ * Subscribe to mailing list routes
+ */
+Route::post('subscribe', [SubscribeController::class, 'store'])->name('subscription.store');
+
+/**
+ * Unsubscribe from mailing list
+ */
+Route::delete('unsubscribe/{subscribedMail}', [SubscribeController::class, 'destroy'])->name('subscription.destroy');
+
+
+
+/**
+ * Admin Routes
+ */
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('admin', function () {
+
+        // Total subscribers (Share the view with only this view)
+        view()->composer(['admin.dashboard'], function ($view) {
+            $totalSubscribers = Subscriber::get()->count();
+            $view->with('totalSubscribers', $totalSubscribers);
+        });
+
+        return view('admin.dashboard');
+    });
+
+    Route::get('subscribers', [AdminController::class, 'index'])->name('admin.index');
+
+    Route::delete('admin/unsubscribe/{subscribedMail}', [AdminController::class, 'destroy'])->name('admin.destroy');
+
+    Route::delete('subscribers/deleteAll', [AdminController::class, 'deleteAll'])->name('admin.deleteAll');
+
+    Route::get('campaign', [CampaignController::class, 'index']);
+
+    Route::post('campaign', [CampaignController::class, 'store'])->name('campaign.store');
+});
+
+
+/**
+ * Run artisan commands from a link button
+ */
+Route::get('/commands', function () {
+    Artisan::call('migrate:refresh');
+    return redirect()->back();
+});
