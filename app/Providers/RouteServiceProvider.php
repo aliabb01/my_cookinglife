6d698ed842c06eebbda5cce4ef2c0c5ccc126491
aliabb01 <aliabbasov21112001@gmail.com>
2021-04-15 19:24:08 +0400

    Admin variable const added to redirect to admin page after login or register

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -22,6 +22,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/ev';
+    public const ADMIN = '/admin';
 
     /**
      * Define your route model bindings, pattern filters, etc.
